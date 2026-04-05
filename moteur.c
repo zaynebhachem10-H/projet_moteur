d1b571b2af38b527433139f0a63eebdbb3e45d34
zaynebhachem10-H <zayneb.hachem.10@gmail.com>
2026-04-05 11:00:39 +0100

    version 4: ajout boucle acceleration

--- moteur.c
+++ moteur.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 int main() {
     float vitesse = 0;
-vitesse= vitesse+10; 
-vitesse= vitesse-3;
-    printf("vitesse= %.2f\n", vitesse);
+int i; 
+for(i=0 ; i<5;i++){
+
+vitesse= vitesse+10; }
+
+    printf("vitesse= %.2f\n", vitesse);} 
     return 0;
 }
